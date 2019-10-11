2e88f8bd14b14d5ad35c4cb7d2f503041adfd47c
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-10-11 14:09:25 +0000

    data

--- sjb_players/lib/scraper.rb
+++ sjb_players/lib/scraper.rb
@@ -10,6 +10,8 @@ class Player
 attr_accessor :name, :position, :town
 end
  
+ 
+ 
 @doc = Nokogiri::HTML(open('http://nychsfl.org/rosters/st-john-the-baptist/'))
  
 # List of Player names
@@ -18,7 +20,7 @@ fb = @doc.css("div.entry-container.fix").each do  |a|
 # binding.pry
 
 
-data = @doc.css(".entry > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td")
+data = @doc.css(".entry > table:nth-child(1) > tbody:nth-child(2) > tr")[2,10]
 
 
  #post-193 > div.entry-container.fix > div > table:nth-child(1) > tbody > tr:nth-child(4)
@@ -49,32 +51,32 @@ puts player.town
 
 # def self.player_info
 # SJB roster- list of players and where they are from and what year they graduate? Get a number of players graduating each year???
-require 'pry'
+# require 'pry'
 
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
 
 
-class Player
-attr_accessor :name, :position, :town
-end
+# class Player
+# attr_accessor :name, :position, :town
+# end
  
-@doc = Nokogiri::HTML(open('http://nychsfl.org/rosters/st-john-the-baptist/'))
+# @doc = Nokogiri::HTML(open('http://nychsfl.org/rosters/st-john-the-baptist/'))
  
-# List of Player names
-data = doc.css(".entry > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td")
- #post-193 > div.entry-container.fix > div > table:nth-child(1) > tbody > tr:nth-child(4)
-#  ".entry > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td"
- binding.pry 
+# # List of Player names
+# data = doc.css(".entry > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td")
+# #post-193 > div.entry-container.fix > div > table:nth-child(1) > tbody > tr:nth-child(4)
+# #  ".entry > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td"
+# binding.pry 
 
-player = Player.new
-player.name = "#{data[2].text} #{data[1].text}"
-player.position = data[4].text
-player.town = data[7].text
+# player = Player.new
+# player.name = "#{data[2].text} #{data[1].text}"
+# player.position = data[4].text
+# player.town = data[7].text
  
-puts player.name
-puts player.position
-puts player.town
+# puts player.name
+# puts player.position
+# puts player.town
  
 # List of Player info:
 #   Position
