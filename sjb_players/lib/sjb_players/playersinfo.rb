ea15b612b56716117e20db46b24b1f165d2bdaad
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-10-12 21:46:21 +0000

    players

---
+++ sjb_players/lib/sjb_players/playersinfo.rb
@@ -0,0 +1,16 @@
+class Sjb::Positions
+  
+  attr_accessor :quarterback, :runningback, :widereciever, :center, :offensiveline, :defensiveline, :linebacker, :cornerback, :safety
+  
+  def initialize(name, town, graduation_year)
+    @name = name
+    @town = town
+    @graduation_year = graduation_year
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+end
